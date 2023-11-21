0eed062f032c62adf31a3c071625f8dfee23e9e9
Serhii Haber <haberserhii@icloud.com>
2023-11-22 01:45:04 +0200

    upd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,17 @@
 import { Box } from '@chakra-ui/react';
+import videoBg from '../assets/videoBg.mp4';
 
 export default function Home() {
   return (
-    <Box
-      bgImage="url('https://images5.alphacoders.com/456/456536.jpg')"
-      bgRepeat={'no-repeat'}
-      bgSize={'cover'}
-      align="center"
-      justify="center"
-      height="150vh"
-      position={'relative'}
-      top={'-150'}
-    ></Box>
+    <Box position={'relative'} top={'-150'}>
+      <video
+        src={videoBg}
+        muted
+        autoPlay
+        loop
+        position={'relative'}
+        top={'-150'}
+      />
+    </Box>
   );
 }
